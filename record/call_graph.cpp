0124b9214f509f3ca8d617c803aefbfd6bec99b4
Stephan Dollberg <stephan.dollberg@gmail.com>
2016-06-01 22:05:08 +0100

    added first call-graph record example

---
+++ record/call_graph.cpp
@@ -0,0 +1,28 @@
+#include <cstdio>
+#include <cstdint>
+
+double foo(double x) {
+	for (int64_t i = 1; i < 100000000; i++) {
+		x = x * 1.0001;
+	}
+
+	return x;
+} 
+
+double bar(double x) {
+	for (int64_t i = 1; i < 50000000; i++) {
+		x = x * 1.0001;
+	}
+
+	return x;
+} 
+
+double master(double x) {
+	return foo(x) + bar(x);
+}
+
+
+int main() {
+	printf("val: %f\n", master(23));
+	printf("val: %f\n", bar(45));
+}
