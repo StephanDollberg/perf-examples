84c720f4219a4d024d17e3a02c39380a15e6b17b
Stephan Dollberg <stephan.dollberg@gmail.com>
2016-06-05 16:46:15 +0100

    added cache-misses stat example

---
+++ stat/cache_misses.cpp
@@ -0,0 +1,33 @@
+#include <cstdio>
+#include <vector>
+#include <list>
+
+template<typename Container>
+Container build() {
+	int test_size = 100000;
+	Container container;
+	for (int i = 0; i < test_size; ++i) {
+		container.push_back(i);
+	}
+	return container;
+}
+
+template<typename Container>
+typename Container::value_type test(Container& container) {
+	auto ret = typename Container::value_type();
+	for (auto&& value : container) {
+		ret += value;
+	}
+	return ret;
+}
+
+
+int main(int argc, char** /* argv */) {
+	if (argc > 1) { // list
+		auto list = build<std::list<int>>();
+		printf("result list: %d\n", test(list));
+	} else { // vector
+		auto vector = build<std::vector<int>>();
+		printf("result vector: %d\n", test(vector));
+	}
+}
