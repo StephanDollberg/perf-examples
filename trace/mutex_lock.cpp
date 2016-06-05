da78fe8af8622d14e4bc7e26ee2877d8699de888
Stephan Dollberg <stephan.dollberg@gmail.com>
2016-06-05 16:45:56 +0100

    added trace mutex example

---
+++ trace/mutex_lock.cpp
@@ -0,0 +1,19 @@
+#include <thread>
+#include <chrono>
+#include <mutex>
+
+
+int main() {
+	std::mutex mut;
+
+	std::thread t([&] {
+		std::lock_guard<std::mutex> lock(mut);
+		std::this_thread::sleep_for(std::chrono::seconds(5));
+	});
+
+	std::this_thread::sleep_for(std::chrono::seconds(1));
+
+	std::lock_guard<std::mutex> lock(mut);
+
+	t.join();
+}
